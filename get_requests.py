4407353ae1697aa8584488a6856048699a4e5c98
kajal-patel97 <57909923+kajal-patel97@users.noreply.github.com>
2019-12-11 19:04:49 +0000

    search postcode and add to file completed

--- get_requests.py
+++ get_requests.py
@@ -1,7 +1,8 @@
 import requests
 
+
 # # build URL
-path_url = 'http://api.postcodes.io/postcodes/'
+# path_url = 'http://api.postcodes.io/postcodes/'
 # argument = 'CR03SW'
 # post_codes = requests.get(path_url + argument)
 #
@@ -21,20 +22,20 @@ path_url = 'http://api.postcodes.io/postcodes/'
 #     print(key, '-->', dict_response['result'][key])
 
 
-#Task
+# Task
 
 ## 1- Play around with API and getting data
-    # This prints out the keys in the get request
-        #print(dict_response.keys())
+# This prints out the keys in the get request
+# print(dict_response.keys())
 
-    #This prints out the status of the website (which inlcudes the URL and argument passed through)
-        #print(dict_response['status'])
+# This prints out the status of the website (which inlcudes the URL and argument passed through)
+# print(dict_response['status'])
 
-    # This prints out the results as a dictionary
-        #print(dict_response['result'])
+# This prints out the results as a dictionary
+# print(dict_response['result'])
 
 # 2 - from a postcode retrieve: lonitude, latitude, nuts, admin ward
-    #This prints out the longitude, latitude, nuts and admin_ward
+# This prints out the longitude, latitude, nuts and admin_ward
 # print(dict_response['result']['longitude'])
 # print(dict_response['result']['latitude'])
 # print(dict_response['result']['nuts'])
@@ -44,19 +45,49 @@ path_url = 'http://api.postcodes.io/postcodes/'
 # 3 - build a function that returns a lat of a postcode
 def postcode_lat():
     ask_postcode = input('What is the postcode you want to find the latitude of?')
+    path_url = 'http://api.postcodes.io/postcodes/'
     result = requests.get(path_url + ask_postcode.strip())
     post_code_dictionary = result.json()
     return post_code_dictionary['result']['latitude']
 
+
 # 4 - build another function that returns the log of a post code
 def postcode_long():
     ask_postcode = input('What is the postcode you want to find the longitude of?')
+    path_url = 'http://api.postcodes.io/postcodes/'
     result = requests.get(path_url + ask_postcode.strip())
     post_code_dictionary = result.json()
     return post_code_dictionary['result']['longitude']
 
 
-
 # 5 - Allows me to search a postcode, and get the following data exported to a TXT file:
 #         - postcode
 #          -lonitude, latitude, nuts, admin ward
+def search_postcode():
+    ask_postcode = input('What is the postcode you would like to search for?')
+    path_url = 'http://api.postcodes.io/postcodes/'
+    result = requests.get(path_url + ask_postcode.strip())
+    post_code_dictionary = result.json()
+
+    # ask for file creation name
+    ask_file = input('Please name the file you would like to insert your postcode data into')
+    file_name = ask_file + '.txt'
+
+    #create the file
+    try:
+        with open(file_name, 'w+') as file_created:
+            file_created.write(f"Postcode: {post_code_dictionary['result']['postcode']}."
+                               f" Longitude: {post_code_dictionary['result']['longitude']}."
+                               f" Latitude: {post_code_dictionary['result']['latitude']}."
+                               f" Nuts: {post_code_dictionary['result']['nuts']}."
+                               f" Admin Ward: {post_code_dictionary['result']['admin_ward']}.")
+    except TypeError as error:
+        print('Please ensure you have all the correct information')
+
+    finally:
+        print('Completed')
+
+
+print(search_postcode())
+
+
